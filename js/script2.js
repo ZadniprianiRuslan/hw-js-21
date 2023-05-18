03d570e99d631228748f8344d9beec4b14742617
ZadniprianiRuslan <110487357+ZadniprianiRuslan@users.noreply.github.com>
2023-05-18 11:37:31 +0300

    hw-10

--- js/script2.js
+++ js/script2.js
@@ -8,3 +8,35 @@ console.log(fruits);
 fruits.splice(2, 1, "Pear", "Banana");
 console.log(fruits);
 // 3) із зміненого масиву створіть повідомлення.
+
+let massage = prompt("Що бажаєте придбати?");
+console.log(massage);
+
+switch (massage) {
+  case fruits[0]:
+    console.log("є");
+    break;
+
+  case fruits[1]:
+    console.log("є");
+    break;
+
+  case fruits[2]:
+    console.log("є");
+    break;
+
+  case fruits[3]:
+    console.log("є");
+    break;
+
+  case fruits[4]:
+    console.log("є");
+    break;
+
+  case fruits[5]:
+    console.log("є");
+    break;
+
+  default:
+    console.log("Нажаль такого продукту немає в наявності");
+}
